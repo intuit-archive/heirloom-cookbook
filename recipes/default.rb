b1b27fb46f63a06652592a606d23c1d35f21ae95
Brett Weaver <brett@weav.net>
2013-04-13 11:57:01 -0700

    adding default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+package node['heirloom']['package_name'] do
+  version node['heirloom']['version']
+end

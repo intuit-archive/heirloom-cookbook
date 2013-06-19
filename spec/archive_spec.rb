61dfc262991caafa89ae84c8a886d046936ba3f7
Dylan Vaughn <dylancvaughn@gmail.com>
2013-06-19 16:05:30 -0700

    adding test for installing a specific version

--- spec/archive_spec.rb
+++ spec/archive_spec.rb
@@ -5,17 +5,31 @@ require spec_dir + '/spec_helper'
 
 describe Archive do
 
-  let(:chef_run) {
-    runner = ChefSpec::ChefRunner.new(
-      :cookbook_path => 'spec/support/cookbooks',
-      :step_into     => ['heirloom_archive']
-    )
-    #Chef::Log.level = :debug
-    runner.converge 'heirloom-tests::default'
-  }
-  
-  it "should execute the heirloom command" do
-    expect(chef_run).to execute_command('heirloom download -i v1.1.1 -n my-project -o /tmp/heirloom')
+  context "LWRP" do
+    let(:chef_run) {
+      runner = ChefSpec::ChefRunner.new(
+        :cookbook_path => 'spec/support/cookbooks',
+        :step_into     => ['heirloom_archive']
+      )
+      #Chef::Log.level = :debug
+      runner.converge 'heirloom-tests::default'
+    }
+    
+    it "should execute the heirloom command" do
+      expect(chef_run).to execute_command('heirloom download -i v1.1.1 -n my-project -o /tmp/heirloom')
+    end
+  end
+
+  context "package install" do
+    let(:chef_run) {
+      runner = ChefSpec::ChefRunner.new
+      runner.node.set['heirloom']['version'] = 'v1.2.3'
+      runner.converge 'heirloom'
+    }
+
+    it "should install a specific version of the heirloom rpm package" do
+      expect(chef_run).to install_package_at_version 'heirloom', 'v1.2.3'
+    end
   end
 
 end

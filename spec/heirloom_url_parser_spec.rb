eb165f24d96e91f47a3a3c435cc58ba3ed1a03d3
Kevin Young <kevin_young@intuit.com>
2013-10-02 14:34:09 -0700

    updates

--- spec/heirloom_url_parser_spec.rb
+++ spec/heirloom_url_parser_spec.rb
@@ -8,33 +8,18 @@ describe HeirloomURLParser do
   end
   
   before do
-    @url = 's3://intu-lc-us-southeast-1/live_community/b1d9ca85a7311bd939ada326a711d7fc33c89204.tar.gz'
-  end
-  
-  it "should return valid uri" do
-    HeirloomURLParser.should_receive(:new).with(@url)
-    HeirloomURLParser.new(@url)
+    @parser = HeirloomURLParser.new('s3://bucket-prefix-us-southeast-1/lc/c89204.tar.gz')
   end
 
   it "should return id" do
-    @heirloom_mock = double 'heirloom parser'
-    HeirloomURLParser.should_receive(:new).with(@url).and_return @heirloom_mock
-    @heirloom_mock.should_receive(:id).and_return("b1d9ca85a7311bd939ada326a711d7fc33c89204")
-  end
-
-  it "should return name" do
-    pending
-    HeirloomURLParser.should_receive(:new).with(@url).and_return(heirloom = double())
-    heirloom.should_receive(:name).and_return("live_community")
-    h = HeirloomURLParser.new(@url)
-    h.name
+    expect(@parser.id).to eq("c89204")
   end
 
   it "should return bucket_prefix" do
-    pending
-    HeirloomURLParser.should_receive(:new).with(@url).and_return(heirloom = double())
-    heirloom.should_receive(:bucket_prefix).and_return("intu-lc")
-    h = HeirloomURLParser.new(@url)
-    h.bucket_prefix
+    expect(@parser.bucket_prefix).to eq("bucket-prefix")
+  end
+
+  it "should return name" do
+    expect(@parser.name).to eq("lc")
   end
 end
